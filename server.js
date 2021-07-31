45157abf38fa47056c4cabefc1128b85e246e353
Isaac Ssemugenyi <isaacpro01@gmail.com>
2021-07-31 11:21:21 +0300

    Add Cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/test', {
     useNewUrlParser: true,
